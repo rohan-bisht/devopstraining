fe8e7f7ed05779cfb7ac63c5b7fda257e6387565
rohan vscode <rohanbisht56@gmail.com>
2025-05-22 22:21:27 +0530

    comment added

--- reverse.py
+++ reverse.py
@@ -1,6 +1,7 @@
+### REVERSE FIRST & LAST NAME ###
 input_name = input ("Enter your name\n")
 print(' '.join((input_name.strip().split())[::-1]))
-
+### REVERSE STRING ###
 input_string = input("Enter your string\n")
 print(input_string[::-1])
 
